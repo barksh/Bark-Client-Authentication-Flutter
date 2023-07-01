6bf7751e7216af90cae6c35a3edac2ff2609e687
wmxpy <wm@mengw.io>
2023-07-01 00:37:33 -0500

    add web authorization

--- lib/src/sign_in/web_auth.dart
+++ lib/src/sign_in/web_auth.dart
@@ -1,8 +1,16 @@
+import 'package:bark_authentication/src/utils/log.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 
 void startWebAuth() async {
-  await FlutterWebAuth2.authenticate(
-    url: "https://auth.bark.sh",
-    callbackUrlScheme: "my-custom-app",
-  );
+  try {
+    await FlutterWebAuth2.authenticate(
+      url: "https://auth.bark.sh",
+      callbackUrlScheme: "my-custom-app",
+    );
+  } on PlatformException catch (e) {
+    logger.debug(e);
+  } catch (e) {
+    logger.debug(e);
+  }
 }
