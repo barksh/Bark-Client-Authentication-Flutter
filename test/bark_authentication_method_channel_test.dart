fb6f4fbb37e6e3207b48b4e331d3e5953cff7ccc
wmxpy <wm@mengw.io>
2023-06-30 23:41:37 -0500

    format and testing fix

--- test/bark_authentication_method_channel_test.dart
+++ test/bark_authentication_method_channel_test.dart
@@ -1,6 +1,6 @@
+import 'package:bark_authentication/src/platform/platform_method_channel.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bark_authentication/bark_authentication_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +9,8 @@ void main() {
   const MethodChannel channel = MethodChannel('bark_authentication');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +19,8 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
--- test/bark_authentication_test.dart
+++ test/bark_authentication_test.dart
@@ -1,19 +1,19 @@
+import 'package:bark_authentication/src/platform/platform_interface.dart';
+import 'package:bark_authentication/src/platform/platform_method_channel.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bark_authentication/bark_authentication.dart';
-import 'package:bark_authentication/bark_authentication_platform_interface.dart';
-import 'package:bark_authentication/bark_authentication_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockBarkAuthenticationPlatform
     with MockPlatformInterfaceMixin
     implements BarkAuthenticationPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final BarkAuthenticationPlatform initialPlatform = BarkAuthenticationPlatform.instance;
+  final BarkAuthenticationPlatform initialPlatform =
+      BarkAuthenticationPlatform.instance;
 
   test('$MethodChannelBarkAuthentication is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelBarkAuthentication>());
@@ -21,7 +21,8 @@ void main() {
 
   test('getPlatformVersion', () async {
     BarkAuthentication barkAuthenticationPlugin = BarkAuthentication();
-    MockBarkAuthenticationPlatform fakePlatform = MockBarkAuthenticationPlatform();
+    MockBarkAuthenticationPlatform fakePlatform =
+        MockBarkAuthenticationPlatform();
     BarkAuthenticationPlatform.instance = fakePlatform;
 
     expect(await barkAuthenticationPlugin.getPlatformVersion(), '42');
